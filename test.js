e8745c67bfde6280675522c1583e9e1d75e21593
Jody Lorist <jodylorist@mbp-van-jody.home>
2021-10-26 19:17:59 +0200

    changed the way i efetch the data from the json file

--- test.js
+++ test.js
@@ -1,6 +1,24 @@
-var test = require('./dataset.json')
+let cleanData = []
 
-for(var i = 0; i < test.length; i++) {
-    console.log(test[i]['Wat is je oogkleur?'] )
-}
+const data = fetch('./dataset.json')
+.then(results => results.json())
+.then(data => data.forEach(function(v, i, obj) {
+    cleanData.push(data[i]['Kaas is ook een zoogdier?'].toString().toLowerCase())
+    cleanData.sort()
+    
+    Object.keys(v).forEach(key => {
+        console.log(key,': ', v[key].toString().toLowerCase())
+        
+    })
+}))
+
+// console.log(cleanData)
+
+
+
+
+
+// for(var i = 0; i < test.length; i++) {
+//     console.log(test[i]['Wat is je oogkleur?'] )
+// }
 
