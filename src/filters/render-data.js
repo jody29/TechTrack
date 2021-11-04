de8afb087371e0b35cdce06b678f27f2b30ed0b6
Jody Lorist <jodylorist@mbp-van-jody.home>
2021-11-04 16:46:05 +0100

    REFACTOR: added some comments

--- src/filters/render-data.js
+++ src/filters/render-data.js
@@ -1,10 +1,10 @@
 function renderData(data, key, text) {
     data.map(obj => {
-        let div = document.querySelector('div')
-        let p = document.createElement('p')
-        div.appendChild(p)
-        p.setAttribute('style', 'color: ' + obj[key])
-        p.textContent = obj[text].toUpperCase()
+        let div = document.querySelector('div') // select the div in the html
+        let p = document.createElement('p') // create P element
+        div.appendChild(p) // append the P element to the div
+        p.setAttribute('style', 'color: ' + obj[key]) // eyecolor will be the text color
+        p.textContent = obj[text].toUpperCase() // text of the P element will be the wind direction
     })  
 }
 
